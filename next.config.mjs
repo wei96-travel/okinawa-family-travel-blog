12bc39341e8352a1a08e712d1ac694d6b426e69a
wei96 <wei96@users.noreply.github.com>
2026-07-20 13:35:23 +0800

    Add short URL to carousel

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
         source: "/blog/okinawa-driving-rest-stops-family",
         destination: "/blog/okinawa-family-drive-toilet-convenience-store",
         permanent: true
+      },
+      {
+        source: "/car-seat",
+        destination: "/blog/okinawa-car-seat-rental-guide",
+        permanent: false
       }
     ];
   }
